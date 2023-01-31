77e2b99df9333756ffc27a118616ed37f2add469
Zachary Walden <zachary.walden@eagles.oc.edu>
2023-01-30 21:55:38 -0600

    7th attempt

--- stock-info/Images/ZwBitmap.cpp
+++ stock-info/Images/ZwBitmap.cpp
@@ -200,9 +200,8 @@ void ZwBitmap::setPixel(uint32_t x, uint32_t y, uint32_t& pixel_data)
 	for(int i = 0; i < 4; i++)
 	{
 		this->data[y][x][i] = ((pixel_data & (0xFF << (this->channel_shift_vals[i] * 8))) >> (this->channel_shift_vals[i] * 8));
-		//LOG("\n");
-		//LOG_INT(this->channel_shift_vals[i]);
-		//LOG_INT((pixel_data & (0xFF << (this->channel_shift_vals[i] * 8)) >> (this->channel_shift_vals[i] * 8)));
+		LOG_INT(this->channel_shift_vals[i]);
+		LOG_INT((pixel_data & (0xFF << (this->channel_shift_vals[i] * 8)) >> (this->channel_shift_vals[i] * 8)));
 	}
 }
 bool ZwBitmap::allocateMemory(int x, int y, int z, uint8_t initial_value)
