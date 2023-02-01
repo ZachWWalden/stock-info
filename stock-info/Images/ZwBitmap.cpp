a40e7dc3ac3c2fe6de572af2eb2c2d6dd19e565d
Zachary Walden <zachary.walden@eagles.oc.edu>
2023-01-31 22:18:40 -0600

    Fix bitmap vertical reversal

--- stock-info/Images/ZwBitmap.cpp
+++ stock-info/Images/ZwBitmap.cpp
@@ -91,7 +91,7 @@ bool ZwBitmap::readBitmap(std::string filename)
 		return false;
 	}
 
-	//this->setChannelShiftValues(color_header.blue_mask, color_header.green_mask, color_header.red_mask, color_header.alpha_mask);
+	this->setChannelShiftValues(color_header.blue_mask, color_header.green_mask, color_header.red_mask, color_header.alpha_mask);
 
 	this->height = info_header.height;
 	this->width = info_header.width;
@@ -110,7 +110,7 @@ bool ZwBitmap::readBitmap(std::string filename)
 		{
 			//read in pixel data
 			bmp_file.read(reinterpret_cast<char*>(&pixel), sizeof(uint32_t));
-			this->setPixel(x, y, pixel);
+			this->setPixel(x, info_header.height - y, pixel);
 		}
 	}
 
