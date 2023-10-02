07a7908d41d06b45add3e59fdefc706e1c7d751f
Zachary Walden <zachary.walden@eagles.oc.edu>
2023-10-02 09:56:29 -0500

    Attempt string overwrite fix

--- stock-info/stock-info-main.cpp
+++ stock-info/stock-info-main.cpp
@@ -351,7 +351,7 @@ int main(int argc, char *argv[]) {
 		pthread_mutex_unlock(&lock);
 		LOG("MAIN: mutex released");
 	//}
-	stocks[i]->getScene(j)->draw();
+	//stocks[i]->getScene(j)->draw();
 	LOG("MAIN: scene drawn");
 	graphics_mgr.draw();
 	//measure time at end
