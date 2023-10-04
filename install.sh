963c3ef67291456304906f8411b868ef494d7e27
Zachary Walden <zachary.walden@eagles.oc.edu>
2023-10-04 15:13:56 -0500

    Attempt to fix perm issues

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
-mkdir /home/$USER/.local/ &&
-mkdir /home/$USER/.local/bin &&
+sudo mkdir /home/$USER/.local/ &&
+sudo mkdir /home/$USER/.local/bin &&
 #libjsoncpp-dev
 #libcurl4
-sudodo apt install libjsoncpp-dev libcurl4 libcurl4-openssl-dev &&
+sudo apt install libjsoncpp-dev libcurl4 libcurl4-openssl-dev &&
 cd stock-info &&
 make &&
 chmod +x stock-info &&
-cp stock-info /home/$USER/.local/bin/
+sudo cp stock-info /home/$USER/.local/bin/
