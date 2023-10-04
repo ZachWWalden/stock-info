e39df1a9dd0239e14b5c0ede0e0e5d7e7e038e89
Zachary Walden <zachary.walden@eagles.oc.edu>
2023-10-04 15:07:44 -0500

    Remove use of 'zww' because ssh screws it up

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/sh
-
-mkdir /home/$USER/.local/bin &&
-#libjsoncpp-dev
-#libcurl4
-sudo apt install libjsoncpp-dev libcurl4 libcurl4-openssl-dev &&
-cd stock-info &&
-make &&
-chmod +x stock-info &&
-cp stock-info /home/$USER/.local/bin/
+if [ $1 != ""]; then
+	mkdir /home/$1/.local/bin &&
+	#libjsoncpp-dev
+	#libcurl4
+	sudo apt install libjsoncpp-dev libcurl4 libcurl4-openssl-dev &&
+	cd stock-info &&
+	make &&
+	chmod +x stock-info &&
+	cp stock-info /home/$1/.local/bin/
+else
+	echo "Please enter your username"
+fi
